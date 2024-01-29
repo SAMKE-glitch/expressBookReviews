5b2c9f1cd3e6b28b89e3af0b1e1e5e339933ec84
SAMKE-glitch <sammythemwa@gmail.com>
2024-01-29 21:30:23 +0300

    edit axios

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -3,7 +3,7 @@ let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
-const axios = require('axios').default;
+const axios = require('axios');
 
 const doesExist = (username)=>{
   let usersWithSameName = users.filter((user)=>{
